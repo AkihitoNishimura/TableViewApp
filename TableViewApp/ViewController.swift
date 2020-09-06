a48189dc3ab89b3032d8a7aae94cdea464a10804
AkihitoNishimura <plumizreop.3@gmail.com>
2020-09-06 13:19:15 +0900

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by 西村映人 on 2020/09/06.
+//  Copyright © 2020 西村映人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
